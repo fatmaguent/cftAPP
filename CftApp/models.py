1d005a70c4c78fc1f98eca3e5563f0366504c6bb
fatmaguent <guentfatma4@gmail.com>
2024-04-15 02:33:16 +0200

    cft

--- CftApp/models.py
+++ CftApp/models.py
@@ -1,9 +1,53 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
+from django.utils import timezone
 
-class Utilisateur(models.Model):
-    nom = models.CharField(max_length=100)
-    mdp = models.CharField(max_length=100)
+class CustomUserManager(BaseUserManager):
+    def _create_user(self, name, password, **extra_fields):
+        if not name:
+            raise ValueError("The given name must be set")
+
+        user = self.model(name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, name=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(name, password, **extra_fields)
+    
+    def create_superuser(self, name, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(name, password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(blank=True, null=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'name'
+    REQUIRED_FIELDS = []  # You can add required fields here other than 'name' and 'password'
 
     class Meta:
-        # Supprimer la ligne ordering si vous n'avez pas besoin de trier par le champ created
-        ordering = ['nom']  # Par exemple, trier par le champ nom
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+    
+    def get_full_name(self):
+        return self.name
+    
+    def get_short_name(self):
+        return self.name
+
--- CftApp/serializers.py
+++ CftApp/serializers.py
@@ -1,8 +1,1 @@
-from rest_framework import serializers
-from .models import Utilisateur
-
-class UtilisateurSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Utilisateur
-        fields = '__all__'  # Vous pouvez spécifier les champs que vous voulez inclure ici
 
