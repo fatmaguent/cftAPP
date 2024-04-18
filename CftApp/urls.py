afccff70db172e65a9fe35ecd58ed85dd3092306
fatmaguent <guentfatma4@gmail.com>
2024-04-18 12:27:07 +0200

    CFT

--- CftApp/urls.py
+++ CftApp/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from .views import MyTokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
  
 urlpatterns = [
     path('home/', views.HomeView.as_view(), name='home'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
