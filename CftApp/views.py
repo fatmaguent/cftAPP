566418c3d72e6e51328ffbc698c5c378917cef95
Fatma <fatma.guent@soprahr.com>
2024-04-24 11:51:28 +0200

    First test

--- CftApp/views.py
+++ CftApp/views.py
@@ -1,13 +1,11 @@
-from django.http import Http404
+from django.http import JsonResponse, Http404
 from django.shortcuts import redirect
 from django.contrib.auth import logout
-from django.views import View  # Corrected import for View
-from django.views.generic import TemplateView  # Necessary import for TemplateView
-
-from rest_framework.decorators import api_view
+from django.views.generic import View, TemplateView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.parsers import JSONParser
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -15,20 +13,30 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-
-        # Add custom claims
-        token['name'] = user.name
+        # Ajout de revendications personnalisées
+        token['name'] = user.username
         return token
 
 class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class =  MyTokenObtainPairSerializer   
-
+    serializer_class = MyTokenObtainPairSerializer
 
 class LogoutView(View):
     def get(self, request, *args, **kwargs):
         logout(request)
-        return redirect('some-redirect-url')  # Replace 'some-redirect-url' with the actual named URL pattern
+        return redirect('some-redirect-url')  # Remplacez 'some-redirect-url' par le nom de modèle d'URL réel
 
 class HomeView(TemplateView):
     template_name = 'home.html'
-################################################################################
+
+class CustomTokenObtainPairView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = MyTokenObtainPairSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data.get('user')
+            refresh = RefreshToken.for_user(user)
+            return JsonResponse({
+                'access': str(refresh.access_token),
+                'refresh': str(refresh)
+            }, status=status.HTTP_200_OK)
+        else:
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
