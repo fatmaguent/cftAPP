dd7d1ba7a5e9085b545a7bca8a287068eef763ab
Fatma <fatma.guent@soprahr.com>
2024-04-29 00:02:51 +0200

    Pipeline django

--- CftApp/urls.py
+++ CftApp/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.urls import path, include
-from rest_framework_simplejwt import views as jwt_views
+from .views import TokenObtainPairAndRefreshView, TokenRefreshView
+
 urlpatterns = [
     path('home/', views.HomeView.as_view(), name ='home'),
-     path('token/', jwt_views.TokenObtainPairView.as_view(), name ='token_obtain_pair'),
-     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name ='token_refresh'),
+    path('logout/', views.LogoutView.as_view(), name ='logout'),
+    path('token/', TokenObtainPairAndRefreshView.as_view(), name ='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name ='token_refresh'),
 ]
--- CftApp/views.py
+++ CftApp/views.py
@@ -3,9 +3,11 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView as BaseTokenRefreshView
 
 class HomeView(APIView):
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request):
         content = {'message': 'Welcome to the JWT Authentication page using React Js and Django!'}
@@ -25,3 +27,13 @@ class LogoutView(APIView):
                 return Response({"error": "Refresh token not provided."}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+class TokenObtainPairAndRefreshView(TokenObtainPairView):
+    def get(self, request, *args, **kwargs):
+        # Allow GET method for token obtain
+        return super().post(request, *args, **kwargs)
+
+class TokenRefreshView(BaseTokenRefreshView):
+    def get(self, request, *args, **kwargs):
+        # Allow GET method for token refresh
+        return super().post(request, *args, **kwargs)
